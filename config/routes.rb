61f4fcb100d44714c9c4dd933f6619ac057305a7
Bruno Rovani <bruno.rovani@btinternet.com>
2020-07-16 14:26:56 +0100

    rails new

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,25 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'tasks', to: 'tasks#index'
 end
+
+Rails.application.routes.draw do
+  get "tasks/:id", to: "tasks#show", as: :task
+end
+
+  Rails.application.routes.draw do
+    get "tasks/new", to: "tasks#new"
+  end
+
+  Rails.application.routes.draw do
+    post "tasks", to: "tasks#create"
+  end
+
+  Rails.application.routes.draw do
+    get "tasks/:id/edit", to: "tasks#edit"
+  end
+
+  Rails.application.routes.draw do
+    patch "tasks/:id", to: "tasks#update"
+  end
+  
